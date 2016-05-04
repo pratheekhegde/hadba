dedad7f54d34c87d23292903d58fd4d630a76bca
Pratheek Hegde <pratheek.hegde@me.com>
2016-05-04 11:09:19 +0530

    [Update] - add student-start subject cards

--- src/app/main/student/start/start.controller.js
+++ src/app/main/student/start/start.controller.js
@@ -10,8 +10,14 @@
     var vm = this;
 
     angular.element(document).ready(function() {
-      toastr.warning('Feedback form of each subject can be filled and submitted only once!','Important',{positionClass: "toast-bottom-right",timeOut: 10000})
-      toastr.info('Comments have been discontinued.','This is sad',{positionClass: "toast-bottom-right",timeOut: 10000})
+      toastr.warning('Feedback form of each subject can be filled and submitted only once!', 'Important', {
+        positionClass: "toast-bottom-left",
+        timeOut: 10000
+      })
+      toastr.info('Comments have been discontinued.', 'This is sad', {
+        positionClass: "toast-bottom-left",
+        timeOut: 10000
+      })
     });
 
 
