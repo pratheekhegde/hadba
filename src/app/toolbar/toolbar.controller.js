3a88a7acb1392f83bc475e0d295a153169c80c2c
Pratheek Hegde <pratheek.hegde@me.com>
2016-05-05 15:49:24 +0530

    [Update] - add clock to toolbar

--- src/app/toolbar/toolbar.controller.js
+++ src/app/toolbar/toolbar.controller.js
@@ -1,165 +1,54 @@
-(function ()
-{
-    'use strict';
+(function() {
+  'use strict';
 
-    angular
-        .module('app.toolbar')
-        .controller('ToolbarController', ToolbarController);
+  angular
+    .module('app.toolbar')
+    .controller('ToolbarController', ToolbarController);
 
-    /** @ngInject */
-    function ToolbarController($rootScope, $mdSidenav, $translate, $mdToast)
-    {
-        var vm = this;
+  /** @ngInject */
+  function ToolbarController($rootScope, $mdSidenav, $translate, $mdToast, $interval) {
+    var vm = this;
+    vm.clock = "";
+    var timer;
 
-        // Data
-        $rootScope.global = {
-            search: ''
-        };
+    // Run a timer everysecond to display the current time (Simulate a clock)
+    timer = $interval(function() {
+      vm.clock = Date.now();
+    }, 1000);
 
-        vm.bodyEl = angular.element('body');
-        vm.userStatusOptions = [
-            {
-                'title': 'Online',
-                'icon' : 'icon-checkbox-marked-circle',
-                'color': '#4CAF50'
-            },
-            {
-                'title': 'Away',
-                'icon' : 'icon-clock',
-                'color': '#FFC107'
-            },
-            {
-                'title': 'Do not Disturb',
-                'icon' : 'icon-minus-circle',
-                'color': '#F44336'
-            },
-            {
-                'title': 'Invisible',
-                'icon' : 'icon-checkbox-blank-circle-outline',
-                'color': '#BDBDBD'
-            },
-            {
-                'title': 'Offline',
-                'icon' : 'icon-checkbox-blank-circle-outline',
-                'color': '#616161'
-            }
-        ];
-        vm.languages = {
-            en: {
-                'title'      : 'English',
-                'translation': 'TOOLBAR.ENGLISH',
-                'code'       : 'en',
-                'flag'       : 'us'
-            },
-            es: {
-                'title'      : 'Spanish',
-                'translation': 'TOOLBAR.SPANISH',
-                'code'       : 'es',
-                'flag'       : 'es'
-            },
-            tr: {
-                'title'      : 'Turkish',
-                'translation': 'TOOLBAR.TURKISH',
-                'code'       : 'tr',
-                'flag'       : 'tr'
-            }
-        };
+    // Data
+    vm.bodyEl = angular.element('body');
 
-        // Methods
-        vm.toggleSidenav = toggleSidenav;
-        vm.logout = logout;
-        vm.changeLanguage = changeLanguage;
-        vm.setUserStatus = setUserStatus;
-        vm.toggleHorizontalMobileMenu = toggleHorizontalMobileMenu;
+    // Methods
+    vm.toggleSidenav = toggleSidenav;
+    vm.toggleHorizontalMobileMenu = toggleHorizontalMobileMenu;
 
-        //////////
+    //////////
 
-        init();
+    init();
 
-        /**
-         * Initialize
-         */
-        function init()
-        {
-            // Select the first status as a default
-            vm.userStatus = vm.userStatusOptions[0];
+    /**
+     * Initialize
+     */
+    function init() {
 
-            // Get the selected language directly from angular-translate module setting
-            vm.selectedLanguage = vm.languages[$translate.preferredLanguage()];
-        }
-
-
-        /**
-         * Toggle sidenav
-         *
-         * @param sidenavId
-         */
-        function toggleSidenav(sidenavId)
-        {
-            $mdSidenav(sidenavId).toggle();
-        }
-
-        /**
-         * Sets User Status
-         * @param status
-         */
-        function setUserStatus(status)
-        {
-            vm.userStatus = status;
-        }
-
-        /**
-         * Logout Function
-         */
-        function logout()
-        {
-            // Do logout here..
-        }
-
-        /**
-         * Change Language
-         */
-        function changeLanguage(lang)
-        {
-            vm.selectedLanguage = lang;
-
-            /**
-             * Show temporary message if user selects a language other than English
-             *
-             * angular-translate module will try to load language specific json files
-             * as soon as you change the language. And because we don't have them, there
-             * will be a lot of errors in the page potentially breaking couple functions
-             * of the template.
-             *
-             * To prevent that from happening, we added a simple "return;" statement at the
-             * end of this if block. If you have all the translation files, remove this if
-             * block and the translations should work without any problems.
-             */
-            if ( lang.code !== 'en' )
-            {
-                var message = 'Fuse supports translations through angular-translate module, but currently we do not have any translations other than English language. If you want to help us, send us a message through ThemeForest profile page.';
-
-                $mdToast.show({
-                    template : '<md-toast id="language-message" layout="column" layout-align="center start"><div class="md-toast-content">' + message + '</div></md-toast>',
-                    hideDelay: 7000,
-                    position : 'top right',
-                    parent   : '#content'
-                });
-
-                return;
-            }
-
-            // Change the language
-            $translate.use(lang.code);
-        }
-
-        /**
-         * Toggle horizontal mobile menu
-         */
-        function toggleHorizontalMobileMenu()
-        {
-            vm.bodyEl.toggleClass('ms-navigation-horizontal-mobile-menu-active');
-        }
     }
+
+    /**
+     * Toggle sidenav
+     *
+     * @param sidenavId
+     */
+    function toggleSidenav(sidenavId) {
+      $mdSidenav(sidenavId).toggle();
+    }
+
+    /**
+     * Toggle horizontal mobile menu
+     */
+    function toggleHorizontalMobileMenu() {
+      vm.bodyEl.toggleClass('ms-navigation-horizontal-mobile-menu-active');
+    }
+  }
 
 })();
